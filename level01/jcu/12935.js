174e252f917f4cb898f2ce62eafcd9ca49e14e56
seeyoujeong <chan3358@naver.com>
2023-12-06 16:08:08 +0900

    feat: level01 해답 추가

---
+++ level01/jcu/12935.js
@@ -0,0 +1,9 @@
+// 제일 작은 수 제거하기
+
+function solution(arr) {
+  const minNumber = Math.min(...arr);
+  const filteredArr = arr.filter((num) => num !== minNumber);
+  return filteredArr.length > 0 ? filteredArr : [-1];
+}
+
+// TODO 가운데 글자 가져오기
--- level01/jcu/12948.js
+++ level01/jcu/12948.js
@@ -3,5 +3,3 @@
 function solution(phone_number) {
   return "*".repeat(phone_number.slice(0, -4).length) + phone_number.slice(-4);
 }
-
-// TODO 제일 작은 수 제거하기
