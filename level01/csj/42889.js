faa222407b5559c38c2023d454aad387e158dd47
Sejin Cha <chasj0326@naver.com>
2023-12-13 21:49:21 +0900

    feat: 42889 수정

--- level01/csj/42889.js
+++ level01/csj/42889.js
@@ -1,12 +1,19 @@
 // 실패율
 
 function solution(N, stages) {
-  const fail = Array.from(
-    { length: N },
-    (_, i) =>
-      stages.filter((stage) => stage === i + 1).length /
-      stages.filter((stage) => stage > i).length
-  );
+  const stageCounts = Array(N + 1).fill(0);
+  stages.forEach((stage) => stageCounts[stage - 1]++);
+
+  let totalChallenged = stages.length;
+
+  const fail = Array.from({ length: N }, (_, i) => {
+    const challenged = totalChallenged;
+    const failed = stageCounts[i];
+    totalChallenged -= failed;
+
+    return failed / challenged;
+  });
+
   return Array.from({ length: N }, (_, i) => i + 1).sort(
     (a, b) => fail[b - 1] - fail[a - 1]
   );
