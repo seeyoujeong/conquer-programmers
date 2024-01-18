acccfeda7917581357b9e254b3aa44892d2a5562
Sejin Cha <chasj0326@naver.com>
2024-01-18 16:32:04 +0900

    feat: level02 수정

--- level02/csj/87946.js
+++ level02/csj/87946.js
@@ -5,19 +5,14 @@ function solution(k, dungeons) {
     if (dungeons.length === 0) {
       return current;
     }
-    const results = [];
-    for (let i = 0; i < dungeons.length; i++) {
-      const [need, cost] = dungeons[i];
-      if (need > fatigue) {
-        results.push(current);
-      } else {
-        const newDeogeons = [...dungeons];
-        newDeogeons.splice(i, 1);
-        results.push(
-          travelDungeon(current + 1, newDeogeons, fatigue - cost)
-        );
-      }
-    }
+    const results = dungeons.map(([need, cost], i) => {
+      if (need > fatigue) return current;
+
+      const newDeogeons = [...dungeons];
+      newDeogeons.splice(i, 1);
+
+      return travelDungeon(current + 1, newDeogeons, fatigue - cost);
+    });
     return Math.max(...results);
   };
 
